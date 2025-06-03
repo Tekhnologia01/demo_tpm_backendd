f47fc1c6929daab6e16d733171c2a399be5edf96
Tekhnologia01 <202914674+Tekhnologia01@users.noreply.github.com>
2025-06-03 11:48:13 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,19 +1,17 @@
 import express from 'express';
 import cors from 'cors';
 import route from './Routes/route.js';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const app = express();
-
 app.use(cors());
 app.use(express.json());
-
 app.use('/', route);
 
 const PORT = process.env.PORT || 3008;
-
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
-
 
 
