a9c776ce13aae953d5f70fa8b462c24dc5bdee9c
Tekhnologia01 <git@tekhnologiaindia.com>
2025-07-30 12:37:26 +0530

    Update database.js

--- src/utils/database.js
+++ src/utils/database.js
@@ -6,7 +6,7 @@ const pool = mysql.createPool({
     host: process.env.DB_HOST || 'tekhnologia-mysql.mysql.database.azure.com',
     user: process.env.DB_USER || 'tekhnologia',
     password: process.env.DB_PASSWORD || 'Royal@123',
-    database: process.env.DB_NAME || 'tpm',
+    database: process.env.DB_NAME || 'tpm_cicd',
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0,
